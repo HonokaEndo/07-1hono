c704d81d999811ebdf53d51ca090a6d8dfc32572
HonokaEndo <honoendo0130@gmail.com>
2024-06-22 18:40:23 +0900

    08-5:ブログ投稿の編集

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,6 @@
         <title>Blog</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
        
@@ -19,6 +18,9 @@
                 <h3>本文</h3>
                 <p class='body'>{{ $post->body }}</p>
             </div>
+        </div>
+        <div class='edit'>
+            <a href="/posts/{{$post->id}}/edit">edit</a>
         </div>
         <div class='footer'>
             <a href="/">戻る</a>
