7a2ea75ec5d4d85e8ff8ce4cd6a616b49857c42b
HonokaEndo <honoendo0130@gmail.com>
2024-07-03 18:11:36 +0900

    変更

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,21 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        
-
-       
-    </head>
     <x-app-layout>
         <x-slot name="header">
             Index
         </x-slot>
-        <body class="antialiased">
-            <br>
+
             <h1>Blog Name</h1>
             <a href="/posts/create">[create]</a>
             <div class='posts'>
@@ -44,8 +32,6 @@
             </script>
             <br>
             ログインユーザー：{{ Auth::user()->name }}
-        </body>
     </x-app-layout>
-</html>
 
 
